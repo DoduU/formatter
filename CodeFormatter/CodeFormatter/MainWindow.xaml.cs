9da79f8f3d5f393e5e6f73e0068d5c26c1153ce8
DoduU <usualvd@gmail.com>
2014-01-20 14:56:12 +0300

    some new

--- CodeFormatter/CodeFormatter/MainWindow.xaml.cs
+++ CodeFormatter/CodeFormatter/MainWindow.xaml.cs
@@ -27,21 +27,62 @@ namespace CodeFormatter
         {
             InitializeComponent();
         }
-
+        string curFile;
+        string file;
         private void btn_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openSqlScript = new OpenFileDialog();
             openSqlScript.ShowDialog();
-            string curFile = openSqlScript.FileName;
-            string file = File.ReadAllText(curFile);
-            Text.Clear();
-            string name = "name";
-            Text.AppendText(name);
-            Text.Foreground = new SolidColorBrush(Colors.Yellow);
-            Text.AppendText(file);
-            Text.Select(2,4);
-            Text.SelectionBrush = new SolidColorBrush(Colors.Red);
+            curFile = openSqlScript.FileName;
+            file = File.ReadAllText(curFile);
+            rtb.Document.Blocks.Clear();
+            rtb.AppendText(file);
+
+
 
         }
+
+
+        private void Color_Format_Clauses()
+        {
+
+
+            rtb.SelectAll();
+            string script = rtb.Selection.Text;
+            int keyWordLength;
+
+
+            string[] clausesSQL = { "select ", "from" };
+                foreach (string clauseCounter in clausesSQL)
+                {
+                    int scriptLength = script.Length;
+                    keyWordLength = clauseCounter.Length;
+                    int clauseIndexStart = 0;
+                    int clauseIndexEnd = 0;
+                    int index = 0;
+                    if(scriptLength>0 &&keyWordLength>0)
+                        while (clauseIndexStart < scriptLength - keyWordLength)
+                        {
+                            TextPointer start_pointer, end_pointer;
+                            clauseIndexStart = file.IndexOf(clauseCounter, index);
+                            if (clauseIndexStart < 0)
+                                break;
+
+                            clauseIndexEnd = clauseIndexStart + keyWordLength;
+                            start_pointer = rtb.Document.ContentStart.GetNextInsertionPosition(LogicalDirection.Forward).GetPositionAtOffset(clauseIndexStart, LogicalDirection.Forward);
+                            end_pointer = rtb.Document.ContentStart.GetNextInsertionPosition(LogicalDirection.Forward).GetPositionAtOffset(clauseIndexEnd+1, LogicalDirection.Forward);
+                            TextRange got = new TextRange(start_pointer, end_pointer);
+                            got.ApplyPropertyValue(TextElement.ForegroundProperty, Brushes.Blue);
+                            got.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Bold);
+                            index = clauseIndexEnd + 1;
+                        }
+                }
+        }
+
+        private void btn_apply_click(object sender, RoutedEventArgs e)
+        {
+            Color_Format_Clauses();
+        }
+
     }
 }
